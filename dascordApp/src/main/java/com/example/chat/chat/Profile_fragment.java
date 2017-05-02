0c19e085026a1b0e52559503dc5a886a0c86d95a
sara <saranordberg@me.com>
2017-05-02 11:44:56 +0200

    Changed layout of profile

--- dascordApp/src/main/java/com/example/chat/chat/Profile_fragment.java
+++ dascordApp/src/main/java/com/example/chat/chat/Profile_fragment.java
@@ -14,6 +14,6 @@ public class Profile_fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
